047e1b32365ea82a65d1597665cc667a98c1b927
Oriol <orma476@vidalibarraquer.net>
2024-04-02 04:29:11 -0700

    Add project files.

---
+++ CustomControl/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SynthwaveLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            synthwaveLabel1 = new SynthwaveLabel();
+            synthwaveLabel2 = new SynthwaveLabel();
+            synthwaveLabel3 = new SynthwaveLabel();
+            synthwaveLabel5 = new SynthwaveLabel();
+            synthwaveLabel4 = new SynthwaveLabel();
+            synthwaveLabel6 = new SynthwaveLabel();
+            SuspendLayout();
+            // 
+            // synthwaveLabel1
+            // 
+            synthwaveLabel1.AutoSize = true;
+            synthwaveLabel1.BackColor = SystemColors.ActiveCaptionText;
+            synthwaveLabel1.DisplayText = "SUS";
+            synthwaveLabel1.EndColor = Color.MediumSpringGreen;
+            synthwaveLabel1.Font = new Font("Segoe Script", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            synthwaveLabel1.ForeColor = Color.Transparent;
+            synthwaveLabel1.GradientAngle = 135;
+            synthwaveLabel1.Location = new Point(80, 47);
+            synthwaveLabel1.Name = "synthwaveLabel1";
+            synthwaveLabel1.RotateAngle = 0;
+            synthwaveLabel1.Size = new Size(176, 106);
+            synthwaveLabel1.StartColor = Color.Magenta;
+            synthwaveLabel1.TabIndex = 0;
+            synthwaveLabel1.Text = "SUS";
+            synthwaveLabel1.Transparent1 = 255;
+            synthwaveLabel1.Transparent2 = 255;
+            // 
+            // synthwaveLabel2
+            // 
+            synthwaveLabel2.AutoSize = true;
+            synthwaveLabel2.DisplayText = "F·R·I·E·N·D·S";
+            synthwaveLabel2.EndColor = Color.Turquoise;
+            synthwaveLabel2.Font = new Font("Ink Free", 47.9999924F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            synthwaveLabel2.ForeColor = Color.Transparent;
+            synthwaveLabel2.GradientAngle = 90;
+            synthwaveLabel2.Location = new Point(308, 138);
+            synthwaveLabel2.Name = "synthwaveLabel2";
+            synthwaveLabel2.RotateAngle = 1;
+            synthwaveLabel2.Size = new Size(410, 79);
+            synthwaveLabel2.StartColor = SystemColors.WindowText;
+            synthwaveLabel2.TabIndex = 1;
+            synthwaveLabel2.Text = "F·R·I·E·N·D·S";
+            synthwaveLabel2.TextAlign = ContentAlignment.MiddleCenter;
+            synthwaveLabel2.Transparent1 = 255;
+            synthwaveLabel2.Transparent2 = 255;
+            // 
+            // synthwaveLabel3
+            // 
+            synthwaveLabel3.AutoSize = true;
+            synthwaveLabel3.BackColor = Color.Teal;
+            synthwaveLabel3.DisplayText = "L";
+            synthwaveLabel3.EndColor = Color.FromArgb(255, 128, 255);
+            synthwaveLabel3.Font = new Font("Ink Free", 47.9999924F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            synthwaveLabel3.ForeColor = Color.Transparent;
+            synthwaveLabel3.GradientAngle = 180;
+            synthwaveLabel3.Location = new Point(80, 243);
+            synthwaveLabel3.Name = "synthwaveLabel3";
+            synthwaveLabel3.RotateAngle = -20;
+            synthwaveLabel3.Size = new Size(72, 79);
+            synthwaveLabel3.StartColor = Color.LimeGreen;
+            synthwaveLabel3.TabIndex = 2;
+            synthwaveLabel3.Text = "L";
+            synthwaveLabel3.Transparent1 = 255;
+            synthwaveLabel3.Transparent2 = 255;
+            // 
+            // synthwaveLabel5
+            // 
+            synthwaveLabel5.AutoSize = true;
+            synthwaveLabel5.BackColor = Color.Teal;
+            synthwaveLabel5.DisplayText = "0";
+            synthwaveLabel5.EndColor = Color.FromArgb(255, 128, 255);
+            synthwaveLabel5.Font = new Font("Ink Free", 47.9999924F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            synthwaveLabel5.ForeColor = Color.Transparent;
+            synthwaveLabel5.GradientAngle = 180;
+            synthwaveLabel5.Location = new Point(158, 243);
+            synthwaveLabel5.Name = "synthwaveLabel5";
+            synthwaveLabel5.RotateAngle = -20;
+            synthwaveLabel5.Size = new Size(71, 79);
+            synthwaveLabel5.StartColor = Color.LimeGreen;
+            synthwaveLabel5.TabIndex = 4;
+            synthwaveLabel5.Text = "0";
+            synthwaveLabel5.Transparent1 = 255;
+            synthwaveLabel5.Transparent2 = 255;
+            // 
+            // synthwaveLabel4
+            // 
+            synthwaveLabel4.AutoSize = true;
+            synthwaveLabel4.BackColor = Color.Teal;
+            synthwaveLabel4.DisplayText = "L";
+            synthwaveLabel4.EndColor = Color.FromArgb(255, 128, 255);
+            synthwaveLabel4.Font = new Font("Ink Free", 47.9999924F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            synthwaveLabel4.ForeColor = Color.Transparent;
+            synthwaveLabel4.GradientAngle = 180;
+            synthwaveLabel4.Location = new Point(235, 243);
+            synthwaveLabel4.Name = "synthwaveLabel4";
+            synthwaveLabel4.RotateAngle = -20;
+            synthwaveLabel4.Size = new Size(72, 79);
+            synthwaveLabel4.StartColor = Color.LimeGreen;
+            synthwaveLabel4.TabIndex = 5;
+            synthwaveLabel4.Text = "L";
+            synthwaveLabel4.Transparent1 = 255;
+            synthwaveLabel4.Transparent2 = 255;
+            // 
+            // synthwaveLabel6
+            // 
+            synthwaveLabel6.AutoSize = true;
+            synthwaveLabel6.BackColor = Color.Indigo;
+            synthwaveLabel6.DisplayText = "W";
+            synthwaveLabel6.EndColor = Color.Cornsilk;
+            synthwaveLabel6.Font = new Font("Marlett", 72F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            synthwaveLabel6.ForeColor = Color.Transparent;
+            synthwaveLabel6.GradientAngle = 180;
+            synthwaveLabel6.Location = new Point(460, 272);
+            synthwaveLabel6.Name = "synthwaveLabel6";
+            synthwaveLabel6.RotateAngle = -5;
+            synthwaveLabel6.Size = new Size(137, 96);
+            synthwaveLabel6.StartColor = Color.DarkMagenta;
+            synthwaveLabel6.TabIndex = 6;
+            synthwaveLabel6.Text = "W";
+            synthwaveLabel6.Transparent1 = 150;
+            synthwaveLabel6.Transparent2 = 200;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(synthwaveLabel6);
+            Controls.Add(synthwaveLabel4);
+            Controls.Add(synthwaveLabel5);
+            Controls.Add(synthwaveLabel3);
+            Controls.Add(synthwaveLabel2);
+            Controls.Add(synthwaveLabel1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private SynthwaveLabel synthwaveLabel1;
+        private SynthwaveLabel synthwaveLabel2;
+        private SynthwaveLabel synthwaveLabel3;
+        private SynthwaveLabel synthwaveLabel5;
+        private SynthwaveLabel synthwaveLabel4;
+        private SynthwaveLabel synthwaveLabel6;
+    }
+}
---
+++ CustomControl/SynthwaveLabel.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace SynthwaveLabel
+{
+
+    public class SynthwaveLabel : System.Windows.Forms.Label
+    {
+
+        private Color color1 = Color.Pink;
+        private Color color2 = Color.LimeGreen;
+        private int color1Transparent = 150;
+        private int color2Transparent = 150;
+        private int angle = 90;
+        private int m_RotateAngle = 0;
+        private String text = "Text";
+
+        //Create Properties  
+        public String DisplayText
+        {
+            get { return text; }
+            set { text = value; Invalidate(); }
+        }
+
+        public Color StartColor
+        {
+            get { return color1; }
+            set { color1 = value; Invalidate(); }
+        }
+
+        public Color EndColor
+        {
+            get { return color2; }
+            set { color2 = value; Invalidate(); }
+        }
+
+        public int Transparent1
+        {
+            get { return color1Transparent; }
+            set
+            {
+                color1Transparent = value;
+                if (color1Transparent > 255)
+                {
+                    color1Transparent = 255;
+                    Invalidate();
+                }
+                else
+                    Invalidate();
+            }
+        }
+
+        public int Transparent2
+        {
+            get { return color2Transparent; }
+            set
+            {
+                color2Transparent = value;
+                if (color2Transparent > 255)
+                {
+                    color2Transparent = 255;
+                    Invalidate();
+                }
+                else
+                    Invalidate();
+            }
+        }
+
+        public int GradientAngle
+        {
+            get { return angle; }
+            set { angle = value; Invalidate(); }
+        }
+
+        public int RotateAngle 
+        { 
+            get { return m_RotateAngle; } 
+            set { m_RotateAngle = value; Invalidate(); }
+        }
+
+        public SynthwaveLabel()
+        {
+            this.ForeColor = Color.Transparent;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Func<double, double> DegToRad = (angle) => Math.PI * angle / 180.0;
+
+            int normalAngle = ((RotateAngle % 360) + 360) % 360;
+            double normaleRads = DegToRad(normalAngle);
+
+            base.OnPaint(e);
+            this.Text = text;
+            Color c1 = Color.FromArgb(color1Transparent, color1);
+            Color c2 = Color.FromArgb(color2Transparent, color2);
+            //draw a string of text label  
+            Brush b = new System.Drawing.Drawing2D.LinearGradientBrush(new Rectangle(0, 0, 50, 50), c1, c2, angle);
+            e.Graphics.RotateTransform(this.RotateAngle);
+            e.Graphics.DrawString(this.Text, this.Font, b, new Point(0, 0));
+            SizeF size = e.Graphics.MeasureString(this.text, this.Font, this.Parent.Width);
+
+            int hSinTheta = (int)Math.Ceiling((size.Height * Math.Sin(normaleRads)));
+            int wCosTheta = (int)Math.Ceiling((size.Width * Math.Cos(normaleRads)));
+            int wSinTheta = (int)Math.Ceiling((size.Width * Math.Sin(normaleRads)));
+            int hCosTheta = (int)Math.Ceiling((size.Height * Math.Cos(normaleRads)));
+
+            int rotatedWidth = Math.Abs(hSinTheta) + Math.Abs(wCosTheta);
+            int rotatedHeight = Math.Abs(wSinTheta) + Math.Abs(hCosTheta);
+
+            this.Width = rotatedWidth;
+            this.Height = rotatedHeight;
+
+            int numQuadrants =
+                (normalAngle >= 0 && normalAngle < 90) ? 1 :
+                (normalAngle >= 90 && normalAngle < 180) ? 2 :
+                (normalAngle >= 180 && normalAngle < 270) ? 3 :
+                (normalAngle >= 270 && normalAngle < 360) ? 4 :
+                0;
+
+            int horizShift = 0;
+            int vertShift = 0;
+
+            if (numQuadrants == 1)
+            {
+                horizShift = Math.Abs(hSinTheta);
+            }
+            else if (numQuadrants == 2)
+            {
+                horizShift = rotatedWidth;
+                vertShift = Math.Abs(hCosTheta);
+            }
+            else if (numQuadrants == 3)
+            {
+                horizShift = Math.Abs(wCosTheta);
+                vertShift = rotatedHeight;
+            }
+            else if (numQuadrants == 4)
+            {
+                vertShift = Math.Abs(wSinTheta);
+            }
+
+            e.Graphics.TranslateTransform(horizShift, vertShift);
+            e.Graphics.RotateTransform(this.RotateAngle);
+
+        }
+    }
+}
